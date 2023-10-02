e804226c569a8a4a9ef47436c2361e292e7d7983
kaareskokebok <109585723+kaareskokebok@users.noreply.github.com>
2023-10-02 11:02:57 +0200

    Add files via upload
    lagt til løsninger til innlæringsdelen i kap1

---
+++ losninger/kap1_skriveTilSkjerm/107_rotter.py
@@ -0,0 +1,8 @@
+
+import math
+print(math.sqrt(4))
+print(math.sqrt(36))
+print(math.sqrt(100))
+print("Kvadratroten til 144 er", math.sqrt(144))
+print(math.sqrt(1234*5))
+print((math.sqrt(6**2+8**2))/2)
