730f16de6da5c020813c592be8b735bf848bdddf
kaare <109585723+kaareskokebok@users.noreply.github.com>
2023-10-06 21:03:49 +0200

    nye løsn

---
+++ losninger/kap1_skrive_til_skjerm/0104_dikt.py
@@ -0,0 +1,6 @@
+
+print("Nå.")
+print("Et meget lite ord.")
+print("Nå.")
+print("Likevel rommer det")
+print("hele evigheten.")
---
+++ losninger/kap1_skrive_til_skjerm/0107_rotter.py
@@ -0,0 +1,10 @@
+
+import math
+
+print(math.sqrt(4))
+print(math.sqrt(25))
+print(math.sqrt(36))
+print(math.sqrt(125-25)*2)
+print("Kvadratroten til 144 er", math.sqrt(144))
+print(math.sqrt(1234*5))
+print((math.sqrt(6**2+8**2))/2)
