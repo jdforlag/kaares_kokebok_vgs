98fc5000e88795146dd6914c3117c23253ab1c41
kaare <109585723+kaareskokebok@users.noreply.github.com>
2023-10-06 20:41:14 +0200

    slette gammel mappe

--- losninger/kap1_skriveTilSkjerm/104_dikt.py
+++
@@ -1,6 +0,0 @@
-
-print("Nå.")
-print("Et meget lite ord")
-print("Nå.")
-print("Likevel rommer det")
-print("hele evigheten.")
--- losninger/kap1_skriveTilSkjerm/107_rotter.py
+++
@@ -1,8 +0,0 @@
-
-import math
-print(math.sqrt(4))
-print(math.sqrt(36))
-print(math.sqrt(100))
-print("Kvadratroten til 144 er", math.sqrt(144))
-print(math.sqrt(1234*5))
-print((math.sqrt(6**2+8**2))/2)
