2b6e710d82d12474f5cba6e5a035f8fb4ff19284
kaare <109585723+kaareskokebok@users.noreply.github.com>
2024-01-03 18:55:34 +0100

    Update 0604_inntekter.py
    Takk til Sian for korreksjon

--- losninger/kap06_lister/0604_inntekter.py
+++ losninger/kap06_lister/0604_inntekter.py
@@ -1,6 +1,12 @@
 inntekter = [3, 2, 1, 5, 3, 3, 8, 10, 7, 7, 4, 6]
 sum_inntekter = 0
+antall_minst5 = 0
+
 for inntekt in inntekter:
+    if inntekt >= 5:
+        antall_minst5 += 1
     print(f"{inntekt} millioner kr")
     sum_inntekter += inntekt
+    
 print(sum_inntekter)
+print(f"Det var {antall_minst5} måneder med minst 5 millioner kroner.")
